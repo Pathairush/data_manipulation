bebd63340f6c97165dcf6b3e8cca02ff9a092a54
Pathairush Seeda <31924148+Pathairush@users.noreply.github.com>
2019-03-27 22:23:26 +0700

    Create SQL_template_pat.sql

---
+++ SQL/SQL_template_pat.sql
@@ -0,0 +1,37 @@
+-- website for SQL reference
+-- https://www.w3schools.com/sql/sql_case.asp 
+
+-- CASE statement
+SELECT OrderID, Quantity,
+CASE WHEN Quantity > 30 THEN "The quantity is greater than 30"
+WHEN Quantity = 30 THEN "The quantity is 30"
+ELSE "The quantity is under 30"
+END AS QuantityText
+FROM OrderDetails; -- for group company type then groupby again
+
+-- aggregatuib over partition and order by
+SELECT id,
+       account_id,
+       standard_qty,
+       DATE_TRUNC('month', occurred_at) AS month,
+       DENSE_RANK() OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS dense_rank,
+       SUM(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS sum_std_qty,
+       COUNT(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS count_std_qty,
+       AVG(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS avg_std_qty,
+       MIN(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS min_std_qty,
+       MAX(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS max_std_qty
+FROM orders
+
+-- window by with alias
+SELECT id,
+       account_id,
+       DATE_TRUNC('year',occurred_at) AS year,
+       DENSE_RANK() OVER account_year_window AS dense_rank,
+       total_amt_usd,
+       SUM(total_amt_usd) OVER account_year_window AS sum_total_amt_usd,
+       COUNT(total_amt_usd) OVER account_year_window AS count_total_amt_usd,
+       AVG(total_amt_usd) OVER account_year_window AS avg_total_amt_usd,
+       MIN(total_amt_usd) OVER account_year_window AS min_total_amt_usd,
+       MAX(total_amt_usd) OVER account_year_window AS max_total_amt_usd
+FROM orders 
+WINDOW account_year_window AS (PARTITION BY account_id ORDER BY DATE_TRUNC('year',occurred_at))
